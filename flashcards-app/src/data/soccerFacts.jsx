156b342d75b4454ac4d9e8bd0be95f539dd5c8fa
Joel <Lianb1@nku.edu>
2023-10-02 22:25:19 -0400

    Updated data list

--- flashcards-app/src/data/soccerFacts.jsx
+++ flashcards-app/src/data/soccerFacts.jsx
@@ -39,7 +39,42 @@ const soccerFacts = [
     {
       question: "Which stadium is known as the 'Theatre of Dreams' and is the home of Manchester United?",
       answer: "Old Trafford"
-    }
+    },{
+      question: "Which country has won the most Copa America titles in South America?",
+      answer: "Uruguay"
+    },
+    {
+      question: " Who is the only player to have won the FIFA World Cup three times as a player??",
+      answer: "Pelé"
+    },
+    {
+      question: "Which club has won the most English Premier League titles?",
+      answer: "Manchester United"
+    },
+    {
+      question: "Who holds the record for the fastest hat-trick in Premier League history?",
+      answer: "Sadio Mané"
+    },
+    {
+      question: "Which stadium is known as the 'Camp Nou' and is the home of FC Barcelona?",
+      answer: "Nou Camp"
+    },
+    {
+      question: "Who is the all-time top scorer for the Spanish national team?",
+      answer: "David Villa"
+    },
+    {
+      question: "Who is known as the 'Fenomeno' in the world of soccer?",
+      answer: "Ronaldo Nazário"
+    },
+    {
+      question: "Which country hosted the 2018 FIFA World Cup?",
+      answer: "Russia"
+    },
+    {
+      question: "Which player holds the record for the most goals scored in a single FIFA World Cup tournament?",
+      answer: "Just Fontaine (13 goals in 1958)"
+    },
   ];
   
   export default soccerFacts;
